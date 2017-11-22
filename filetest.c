1d067f7c301ca36112ace2f45f6372d251c6d22d
notegrapher <notegrapher@gmail.com>
2017-11-22 12:36:59 +0000

    file I/O

---
+++ filetest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(void){
+  FILE * in_fp = fopen("input.txt", "r");
+  FILE * out_fp = fopen("output.txt", "w");
+  char buffer[100];
+  while(fscanf(in_fp, "%s", buffer) != EOF){
+    fprintf(out_fp, "%s ", buffer);
+  }
+  fclose(in_fp);
+  fclose(out_fp);
+}
