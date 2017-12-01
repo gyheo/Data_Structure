db5cac09df468564e8d5a42495fc6992e4ef68fa
Kunyoung Heo <notegrapher@gmail.com>
2017-12-01 10:32:11 +0000

    재귀함수 기초

--- sumRecursion.c
+++ sumRecursion.c
@@ -4,7 +4,14 @@
 int recur(int n);
 void main(void){
 	int result = recur(10);
-
+	
+	// int result = recur(1000000);
+	/*
+		위의 경우 백만번 호출..
+		stack 영역이 무진장 커져서 프로세스가 다른 메모리를 침범할 가능성이 ↑
+		(중요) callback에 대한 제한을 두고 있음
+		Segmentation fault (core dumped) 오류 발생
+	*/
 	printf("The recursion sum is %d \n", result);
 
 
